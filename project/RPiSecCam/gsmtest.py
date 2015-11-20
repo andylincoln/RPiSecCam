81a252ccc448009e50d285ec5b45135759c820b8
Andrew Lincoln <andrewlincoln11@gmail.com>
2015-11-20 15:40:17 -0500

    Text message reading

--- project/RPiSecCam/gsmtest.py
+++ project/RPiSecCam/gsmtest.py
@@ -5,6 +5,15 @@ gsm = GSMModem()
 print "All"
 print gsm.getAllMessagesByStatus("ALL")
 
+print "Get message @ index 3"
+print gsm.getSMS(3)
+
+print "Get message count"
+print gsm.getAllMessageCount()
+
+print "Get last message"
+print gsm.getLastMessage()
+
 #print "Read"
 #print gsm.getAllMessagesByStatus("REC READ")
 #print "\n"
