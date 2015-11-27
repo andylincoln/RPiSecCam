b10e48b4d9cc41d92e517f80e29750c2486e692b
Andrew Lincoln <andrewlincoln11@gmail.com>
2015-11-27 14:43:35 -0500

    Text message reading as Message class

--- project/RPiSecCam/gsmtest.py
+++ project/RPiSecCam/gsmtest.py
@@ -1,18 +1,28 @@
 from notificationcontroller import GSMModem
+from notificationcontroller import Message
 
 gsm = GSMModem()
 
 print "All"
 print gsm.getAllMessagesByStatus("ALL")
 
-print "Get message @ index 3"
-print gsm.getSMS(3)
-
 print "Get message count"
 print gsm.getAllMessageCount()
 
 print "Get last message"
-print gsm.getLastMessage()
+lastMessageRaw = gsm.getLastMessage()
+
+print lastMessageRaw
+print "Length: " + str(len(lastMessageRaw))
+
+for string in lastMessageRaw:
+    print string
+
+message = Message(lastMessageRaw)
+print message.status
+print message.content
+print message.phoneNumber
+print message.timestamp
 
 #print "Read"
 #print gsm.getAllMessagesByStatus("REC READ")
