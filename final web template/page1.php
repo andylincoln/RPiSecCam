01f6b5ece7e4279893e6d62be9bec42dec04b3eb
Andrew Lincoln <andrewlincoln11@gmail.com>
2015-11-27 19:47:54 -0500

    Fixed page

--- final web template/page1.php
+++ final web template/page1.php
@@ -1,24 +1,18 @@
-<?PHP
-session_start();
-if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
-        header ("Location: login3.php");
-}
+<?php
+    session_start();
+    if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
+        header ("Location: login.php");
+    }
 ?>
-
-	<html>
-	<head>
-	<title>IOT Device Management Interface</title>
-    <?php  include  'head.php';
-      head();
-    ?>
+<html>
+    <head>
+	    <title>IOT Device Management Interface</title>
+        <?php  include  'head.php'; ?>
 	</head>
 	<body>
-        <div class="container">
-            <div class="container">
+        <div class="container-fluid">
+            <div class="col-lg-12 col-md-12 col-sm-12">
 	            <h1>User Logged in</h1>
-
-                <?PHP   print ("<p>Add your device management code here</p>");
-                print ("<p>I recommend a combination of PHP and python scripts</p>");?>
                  <a href="page2.php" class="btn btn-primary">Log out</a>
             </div>
         </div>
@@ -27,4 +21,4 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 
 
 	</body>
-	</html>
+</html>
